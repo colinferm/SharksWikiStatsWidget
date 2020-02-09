fb349340edb96ad04e07143e45d7ea828bd3e929
Colin <colinferm@users.noreply.github.com>
2020-02-09 12:18:25 +0100

    Added check for element array

--- js/statsshark.js
+++ js/statsshark.js
@@ -59,30 +59,38 @@ var SFS = SFS || {
             hoverHandler: function(evt, elm) {
             	if (elm && gtag) {
             		console.log(elm);
-            		var chartConfig = elm[0]._chart.config;
-            		var title = chartConfig.options.title.text;
-            		var chartType = chartConfig.type;
-            		//console.log("clicked on: " + chartType);
-            		//console.log("Title: " + title);
-            		gtag('event', 'chart_hover', {
-            			'event_category': chartType,
-            			'event_label': title
-          			});
+            		if (elm.length > 0) {
+	            		var chartConfig = elm[0]._chart.config;
+	            		var title = chartConfig.options.title.text;
+	            		var chartType = chartConfig.type;
+	            		//console.log("clicked on: " + chartType);
+	            		//console.log("Title: " + title);
+	            		gtag('event', 'chart_hover', {
+	            			'event_category': chartType,
+	            			'event_label': title
+	          			});
+          			} else {
+          				gtag('event', 'chart_hover');
+        				}
             	}
             },
             
             clickHandler: function(evt, elm) {
             	if (elm && gtag) {
 	            	console.log(elm);
-            		var chartConfig = elm[0]._chart.config;
-            		var title = chartConfig.options.title.text;
-            		var chartType = chartConfig.type;
-            		//console.log("clicked on: " + chartType);
-            		//console.log("Title: " + title);
-            		gtag('event', 'chart_click', {
-            			'event_category': chartType,
-            			'event_label': title
-          			});
+	            	if (elm.length > 0) {
+	            		var chartConfig = elm[0]._chart.config;
+	            		var title = chartConfig.options.title.text;
+	            		var chartType = chartConfig.type;
+	            		//console.log("clicked on: " + chartType);
+	            		//console.log("Title: " + title);
+	            		gtag('event', 'chart_click', {
+	            			'event_category': chartType,
+	            			'event_label': title
+	          			});
+	          		} else {
+	          			gtag('event', 'chart_click');
+	          		}
             	}
             }
         },
